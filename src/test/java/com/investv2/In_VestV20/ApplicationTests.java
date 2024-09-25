18819ac602a6a31cd99be75644992c726dd6fe7c
castonmcdonald <castonmcdonald@gmail.com>
2024-09-25 16:39:41 -0500

    Setup spring initialzr

---
+++ src/test/java/com/investv2/In_VestV20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investv2.In_VestV20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
